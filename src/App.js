6dc4f7fec11c9722308beda4e8b2b1aafd2518e0
glznn <gsm004@ucsd.edu>
2025-07-28 17:45:09 -0700

    Frutiger portfolio, first work in progress

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import {channelData} from './data/channelData.js';
+import { GrMail } from "react-icons/gr";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app__menu">
+        <ul className="app__channels">
+          {channelData.map ((channel, key) => {
+            return (
+              <li
+                key={key}
+                className="app__channel"
+              >
+                <img src={channel.image}/>
+                <h1>{channel.name}</h1>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+      <div className="app__content">
+        <button className="app__button">Don't tap the glass!</button>
+        <h1>Glenn Montepiedra </h1>
+      </div>
+      <div className="app__bottom">
+        <div className="app__leftBlock">
+          <div className="app__leftPocket"/>
+        </div>
+        
+        <div className="app__centerBlock">
+
+        </div>
+        <div className="app__rightBlock">
+          <div className="app__rightPocket"/>
+            <div className="app__mailContainer">
+              <div className="app__mail">
+                <GrMail/>
+              </div>
+            </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/data/channelData.js
@@ -0,0 +1,42 @@
+export const channelData = [
+    {
+        name:"Bis Blue",
+        image: '/images/bis-blue-preview.png'
+    },
+    {
+        name: "Exedra",
+        image: '/images/exedra-preview.png'
+    },
+    {
+        name: "Credits",
+        image:'',
+    },
+    {
+        name:"Placeholder",
+        image: '',
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+]
