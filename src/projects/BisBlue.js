e6ecc3879fa859ef8721660c7441c3f2cdc31b74
glznn <gsm004@ucsd.edu>
2025-08-25 13:23:33 -0700

    added text to project pages draft

--- src/projects/BisBlue.js
+++ src/projects/BisBlue.js
@@ -5,10 +5,21 @@ import BottomChannel from '../components/BottomChannel'
 function BisBlue() {
   return (
     <div className="BisBlue">
+      <div className="bb__layout">
         <div className="bb__image">
           <img src="/images/bis-blue-preview.png"/>
         </div>
-        <BottomChannel/>
+        <div className="bb__content">
+          <h1> classic, timeless design </h1>
+          <h1> sleek user interface </h1>
+          <h1> integrated with shopify </h1>
+          <h1> mobile version </h1>
+          <h1> simple modern aesthetic </h1>
+        </div>
+      </div>
+
+      <BottomChannel/>
+
     </div>
   )
 }
--- src/projects/Exedra.js
+++ src/projects/Exedra.js
@@ -5,10 +5,17 @@ import BottomChannel from '../components/BottomChannel'
 function Exedra() {
   return (
     <div className="Exedra">
+      <div className="ex__layout">
         <div className="ex__image">
           <img src="/images/exedra-preview.png"/>
         </div>
+        <div className="ex__content">
+
+        </div>
+      </div>
+
         <BottomChannel/>
+        
     </div>
   )
 }
